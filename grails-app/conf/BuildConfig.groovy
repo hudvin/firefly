dd726ef4dc42a65230b205702744fd4fb101a0f7
hudvin <hudvin@gmail.com>
2013-05-09 14:33:51 +0300

    twitter bootstrap plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,6 +50,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
+        runtime ":twitter-bootstrap:2.2.1"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
