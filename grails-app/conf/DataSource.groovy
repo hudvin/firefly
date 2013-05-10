97d1b7598a173b4672df4561c501f8d2b1a31a7f
hudvin <hudvin@gmail.com>
2013-05-10 12:41:12 +0300

    db update instead of drop-create

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost/firefly?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
